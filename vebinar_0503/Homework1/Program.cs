e73c0d3be61e2629f7b0d014f9e210e8a7aec541
black_nuar <hasaevsuliman6@gmail.com>
2023-03-07 17:41:25 +0300

    Homework to seminar three

---
+++ vebinar_0503/Homework1/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+
+
+string s = number.ToString();
+char [] ar = s.ToCharArray();
+Array.Reverse(ar);
+s = new string(ar);
+int a = Convert.ToInt32(s);
+
+if (number == a) System.Console.WriteLine("yes");
+else System.Console.WriteLine("No");
---
+++ vebinar_0503/Homework2/Program.cs
@@ -0,0 +1,11 @@
+﻿int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1));
+
+System.Console.WriteLine(result);
